93d23722e8ff49ea4d452763fa7ef62735a97f29
Jose Cheble <josecheble@gmail.com>
2025-06-15 17:24:00 -0300

    Update TreeTest.java

--- test/Models/TreeTest.java
+++ test/Models/TreeTest.java
@@ -1,5 +1,17 @@
 package test.Models;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.lang.reflect.Field;
+import Models.Tree;
+
 public class TreeTest {
-    
+
+    @Test
+    public void testCantidadImagenesEs7() throws Exception {
+        Field imgsField = Tree.class.getDeclaredField("imgs");
+        imgsField.setAccessible(true);
+        Object[] imgs = (Object[]) imgsField.get(null); // null porque es static
+        assertEquals(7, imgs.length);
+    }
 }
