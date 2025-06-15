45574c8f80609aca874b13a40be99768d4fbc05c
Jose Cheble <josecheble@gmail.com>
2025-06-15 17:47:50 -0300

    Update TreeTest.java

--- test/Models/TreeTest.java
+++ test/Models/TreeTest.java
@@ -9,7 +9,7 @@ public class TreeTest {
 
     @Test
     public void testCantidadImagenesEs7() throws Exception {
-        Field imgsField = Tree.class.getDeclaredField("imgs");
+        Field imgsField = Tree.class.getDeclaredField("treeImags");
         imgsField.setAccessible(true);
         Object[] imgs = (Object[]) imgsField.get(null); // null porque es static
         assertEquals(7, imgs.length);
